b84041ee8ee35a78481b5cb99ebc10b9f58209a2
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-06-06 08:12:19 +0530

    dsfsdf

--- frontend/src/Components/Userdetails/Userdetails.jsx
+++ frontend/src/Components/Userdetails/Userdetails.jsx
@@ -1,8 +1,28 @@
 // import React from 'react'
-
+import { useState } from "react";
 import Navbar from "../Navbar/Navbar"
+import axios from "axios"
+import { useEffect } from "react";
+
+const URL = 'http://localhost:5000/users'
+
+const fetchHandler = async () => {
+    return await axios.get(URL).then((res)=>res.data);
+}
 
 function Userdetails() {
+
+    const [users, SetUsers] = useState();
+
+    useEffect(() => {
+        fetchHandler().then((data) => SetUsers(data.users));
+    },[])
+
+
+
+
+
+
   return (
     <div>
         <Navbar />
